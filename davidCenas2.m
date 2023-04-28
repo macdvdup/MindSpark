b3540abb040c14a8aa2e0219ca96200f38b71f59
macdvdup <up201906607@edu.fe.up.pt>
2023-04-28 14:28:45 +0100

    featureExtration
    Têm erros a corrigir em termos das matrizes

---
+++ davidCenas2.m
@@ -0,0 +1,102 @@
+clear; close all;
+%addpath 'D:\Neuro\Projeto\MindSpark\functions'
+
+% Load the EEG data and preprocess it as needed
+eeg_signal_ini = csvread("my_matrix.csv");
+Fs = 256; % Sampling frequency
+eeg_signal_ini = eeg_signal_ini';
+
+eeg_signal = eeg_signal_ini(:,1:4) - eeg_signal_ini(:,5);
+
+% Pre-Processing channel signals
+cutOffFreqs = [0.5, 80];
+eeg_signal = preProcessEEG(eeg_signal, cutOffFreqs, Fs);
+
+% Set the interval duration in seconds
+interval_duration = 2;
+
+% Calculate the number of samples in each interval
+interval_length = round(interval_duration * Fs);
+
+
+% Segment the EEG data into epochs
+epoch_length = 2; % in seconds
+epoch_samples = epoch_length * Fs; % fs is the sampling rate
+n_epochs = floor(length(eeg_signal) / epoch_samples);
+
+% Define the frequency bands of interest
+bands = [8 12; 12 30; 30 80]; % alpha, beta, gamma
+
+% Initialize matrices to store the feature values
+E = zeros(n_epochs, size(bands,1), size(eeg_signal,2));
+ER = zeros(n_epochs, size(bands,1), size(eeg_signal,2));
+EE = zeros(n_epochs, size(bands,1), size(eeg_signal,2));
+DE = zeros(n_epochs, size(bands,1), size(eeg_signal,2));
+PSD = zeros(n_epochs, size(eeg_signal,2));
+ASM = zeros(n_epochs, size(eeg_signal,2));
+RASM = zeros(n_epochs, size(eeg_signal,2));
+DASM = zeros(n_epochs, size(eeg_signal,2));
+
+% Loop over epochs and compute the features for each band and channel
+for i = 1:n_epochs
+    % Extract the current epoch
+    epoch = eeg_signal(:,(i-1)*epoch_samples+1 : i*epoch_samples);
+    
+    % Apply a window function to the epoch
+    w = hamming(epoch_samples);
+    epoch = epoch .* w;
+    
+    % Compute the power spectral density of the epoch
+    [psd, f] = pwelch(epoch, w, epoch_samples/2, [], Fs);
+    PSD(i,:,:) = psd;
+
+    % Loop over the frequency bands and compute the features for each channel
+    for j = 1:size(bands, 1)
+        % Find the indices of the frequencies in the current band
+        ind = (f >= bands(j,1)) & (f <= bands(j,2));
+        % Compute the energy in the band for each channel
+        energy = sum(psd(ind,:), 1);
+        
+        % Compute the total energy in the range of interest
+        total_energy = sum(psd(:,:), 1);
+        
+        % Compute the energy ratio for each channel
+        er = energy ./ total_energy;
+        
+        % Compute the energy entropy for each channel
+        ee = -sum((energy ./ sum(energy)) .* log2(energy ./ sum(energy)), 1);
+        
+        % Compute the differential entropy for each channel
+        de = -sum((psd(ind,:) ./ sum(psd(ind,:))) .* log2(psd(ind,:) ./ sum(psd(ind,:))), 1);
+        
+        % Store the feature values in the matrices
+        E(i,j,:) = energy;
+        ER(i,j,:) = er;
+        EE(i,j,:) = ee;
+        DE(i,j,:) = de;
+        
+        % Compute the asymmetry measures for the alpha band and each channel
+        if j == 1 % alpha band
+            left_channels = [1 2]; % TP9 and AF7
+            right_channels = [3 4]; % TP10 and AF8
+            
+            % Compute the mean alpha power in each hemisphere
+            alpha_power_left = mean(psd(ind,left_channels), 1);
+            alpha_power_right = mean(psd(ind,right_channels), 1);
+            
+            % Compute the total alpha power for each hemisphere
+            alpha_power_total_left = sum(psd(ind,left_channels), 1);
+            alpha_power_total_right = sum(psd(ind,right_channels), 1);
+            
+            % Compute the asymmetry measures
+            asm = (alpha_power_left - alpha_power_right) ./ (alpha_power_total_left + alpha_power_total_right);
+            rasm = (alpha_power_left - alpha_power_right) ./ (alpha_power_left + alpha_power_right);
+            dasm = abs(alpha_power_left - alpha_power_right);
+            
+            % Store the asymmetry measures in the matrices
+            ASM(i,:) = asm;
+            RASM(i,:) = rasm;
+            DASM(i,:) = dasm;
+        end
+    end
+end
--- differentialEntropy.m
+++ differentialEntropy.m
@@ -1,40 +1,47 @@
-% Load the EEG data and preprocess it as needed
-
-% Segment the EEG data into epochs
-epoch_length = 2; % in seconds
-epoch_samples = epoch_length * fs; % fs is the sampling rate
-n_epochs = floor(length(eeg_signal) / epoch_samples);
-
-% Define the frequency bands of interest
-bands = [8 12; 12 30; 30 80]; % alpha, beta, gamma
-
-% Loop over epochs and compute the differential entropy for each band
-for i = 1:n_epochs
-    % Extract the current epoch
-    epoch = eeg_signal((i-1)*epoch_samples+1 : i*epoch_samples, :);
+%% hjh
+function [DE, da] = extractFeatures(eeg_signal, epoch_length, sampling_freq)
     
-    % Apply a window function to the epoch
-    w = hamming(epoch_samples);
-    epoch = epoch .* w;
+    % epoch_length  in seconds
+
+    % Segment the EEG data into epochs
+    epoch_samples = epoch_length * sampling_freq; % fs is the sampling rate
+    n_epochs = floor(length(eeg_signal) / epoch_samples);
     
-    % Compute the power spectral density of the epoch
-    [psd, f] = pwelch(epoch, w, epoch_samples/2, [], fs);
+    % Define the frequency bands of interest
+    alpha_bands = [8,12]
+    beta_bands = [12 30]
+    gamma_bands = [30 55]
+    bands = [alpha_bands; beta_bands; gamma_bands]; % alpha, beta, gamma
     
-    % Loop over the frequency bands and compute the differential entropy
-    for j = 1:size(bands, 1)
-        % Find the indices of the frequencies in the current band
-        ind = (f >= bands(j,1)) & (f <= bands(j,2));
+    % Loop over epochs and compute the differential entropy for each band
+    for i = 1:n_epochs
+        % Extract the current epoch
+        epoch = eeg_signal((i-1)*epoch_samples+1 : i*epoch_samples, :);
         
-        % Compute the total power in the current band
-        total_power = sum(psd(ind));
+        % Apply a window function to the epoch
+        w = hamming(epoch_samples);
+        epoch = epoch .* w;
         
-        % Normalize the power in the current band by the total power in the frequency range of interest
-        relative_power = total_power / sum(psd(f>=1 & f<=80));
+        % Compute the power spectral density of the epoch
+        [psd, f] = pwelch(epoch, w, epoch_samples/2, [], fs);
         
-        % Compute the differential entropy for the current band
-        H(j,i) = -sum(relative_power .* log2(relative_power));
+        % Loop over the frequency bands and compute the differential entropy
+        for j = 1:size(bands, 1)
+            % Find the indices of the frequencies in the current band
+            ind = (f >= bands(j,1)) & (f <= bands(j,2));
+            
+            % Compute the total power in the current band
+            total_power = sum(psd(ind));
+            
+            % Normalize the power in the current band by the total power in the frequency range of interest
+            relative_power = total_power / sum(psd(f>=1 & f<=80));
+            
+            % Compute the differential entropy for the current band
+            DE(j,i) = -sum(relative_power .* log2(relative_power));
+        end
     end
+    
+    % Compute the mean differential entropy across epochs for each band
+    mean_H = mean(H, 2);
 end
 
-% Compute the mean differential entropy across epochs for each band
-mean_H = mean(H, 2);
